60f8e6ac9797287c367a7645b37c6b68c61cbe6a
Viktor Kovacs <viktorkovacs@gmail.com>
2020-10-26 08:07:08 +0100

    Clean up python resource compiler.

--- Sources/AddOnResources/RFIX.win/CompileResources.py
+++ Sources/AddOnResources/RFIX.win/CompileResources.py
@@ -5,21 +5,25 @@ import subprocess
 currentDir = os.path.dirname (os.path.abspath (__file__))
 os.chdir (currentDir)
 
-devKitPath = os.path.abspath (sys.argv[1])
-resourceObjectsPath = os.path.abspath (sys.argv[2])
-resultResFilePath = os.path.abspath (sys.argv[3])
+if len (sys.argv) != 5:
+	print ('Usage: CompileResources.py <languageCode> <devKitPath> <resourceObjectsPath> <resultResourceFilePath>')
+	sys.exit (1)
 
-addOnResourcesPath = os.path.dirname (currentDir)
+languageCode = sys.argv[1]
+devKitPath = os.path.abspath (sys.argv[2])
+resourceObjectsPath = os.path.abspath (sys.argv[3])
+resultResourceFilePath = os.path.abspath (sys.argv[4])
+
+resourcesPath = os.path.dirname (currentDir)
 resConvPath = os.path.join (devKitPath, 'Support', 'Tools', 'Win', 'ResConv.exe')
-rintFolder = os.path.join (addOnResourcesPath, 'RINT');
-rfixFolder = os.path.join (addOnResourcesPath, 'RFIX');
-imagesFolder = os.path.join (rfixFolder, 'Images')
 
-for fileName in os.listdir (rintFolder):
+# Compile localized resources
+locResourcesFolder = os.path.join (resourcesPath, 'RINT');
+for fileName in os.listdir (locResourcesFolder):
 	extension = os.path.splitext (fileName)[1]
 	if extension != '.grc':
 		break
-	grcFilePath = os.path.join (rintFolder, fileName)
+	grcFilePath = os.path.join (locResourcesFolder, fileName)
 	rc2Path = os.path.join (resourceObjectsPath, fileName + '.rc2')
 	result = subprocess.call ([
 		resConvPath,
@@ -35,12 +39,15 @@ for fileName in os.listdir (rintFolder):
 	if result != 0:
 		print ('Failed to compile resource: ' + fileName)
 		sys.exit (1)
-	
-for fileName in os.listdir (rfixFolder):
+
+# Compile fix resources
+fixResourcesFolder = os.path.join (resourcesPath, 'RFIX');
+imageResourcesFolder = os.path.join (fixResourcesFolder, 'Images')
+for fileName in os.listdir (fixResourcesFolder):
 	extension = os.path.splitext (fileName)[1]
 	if extension != '.grc':
 		break
-	grcFilePath = os.path.join (rfixFolder, fileName)
+	grcFilePath = os.path.join (fixResourcesFolder, fileName)
 	rc2Path = os.path.join (resourceObjectsPath, fileName + '.rc2')
 	result = subprocess.call ([
 		resConvPath,
@@ -50,7 +57,7 @@ for fileName in os.listdir (rfixFolder):
 		'-T',
 		'W',
 		'-w', '2',
-		'-p', imagesFolder,
+		'-p', imageResourcesFolder,
 		'-q', 'utf8', '1252',
 		'-i', grcFilePath,
 		'-o', rc2Path
@@ -58,14 +65,15 @@ for fileName in os.listdir (rfixFolder):
 	if result != 0:
 		print ('Failed to compile resource: ' + fileName)
 		sys.exit (1)
-		
+
+# Compile native resources
 result = subprocess.call ([
 	'rc',
 	'/i', os.path.join (devKitPath, 'Support', 'Inc'),
 	'/i', os.path.join (devKitPath, 'Support', 'Modules', 'DGLib'),
 	'/i', resourceObjectsPath,
-	'/fo', resultResFilePath,
-	os.path.join (addOnResourcesPath, 'RFIX.win', 'AddOnMain.rc2')
+	'/fo', resultResourceFilePath,
+	os.path.join (resourcesPath, 'RFIX.win', 'AddOnMain.rc2')
 ])
 if result != 0:
 	print ('Failed to compile native resource')
