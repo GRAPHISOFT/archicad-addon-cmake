9df5835ce499f545f106a7c34f24a9cdce6bb380
Mihalcea Bogdan Daniel <dani_mihalcea@hotmail.com>
2021-08-10 18:59:36 +0300

    better argparsing

--- Sources/AddOnResources/Tools/AddModules.py
+++ Sources/AddOnResources/Tools/AddModules.py
@@ -12,26 +12,21 @@ DEBUG = True
 
 
 class WinModulesCollector(object):
-    def __init__(
-        self, devKitPath, includePath, includeLibPath, cmakeFilePath, vcxprojectFolder
-    ):
-        self.devKitPath = os.path.join(
-            devKitPath
-        )  # - The path to the Dev Kit Directory
-        self.includePath = includePath
-        self.includeLibPath = includeLibPath
+    def __init__(self, devKitPath, cmakeFilePath, vcxprojectFolder):
+        # - The path to the Dev Kit Directory
+        self.devKitPath = os.path.join(devKitPath)
         # - The path to the CMakeList.txt CMAKE_CURRENT_LIST_DIR
-        self.cmakeFilePath = os.path.join(cmakeFilePath)
+        self.cmakeFilePath = os.path.join(cmakeFilePath, "CMakeLists.txt")
         # - The path to the AddOn.vcxprojx CMAKE_CURRENT_BINARY_DIR
-        self.vcxprojectFilePath = os.path.join(vcxprojectFolder, 'AddOn.vcxprojx')
+        self.vcxprojectFilePath = os.path.join(vcxprojectFolder, "AddOn.vcxprojx")
         # {Devkit}\Support\Mopdules
-        self.MODULESPATH = os.path.join(self.devKitPath, 'Support\\Modules')
+        self.MODULESPATH = os.path.join(self.devKitPath, "Support\\Modules")
         # The list of all modules added from archicad library
         self.modulesList = self.GetExistingProjectModules()
 
     def Namespace(self, element):
-        m = re.match(r'\{.*\}', element.tag)
-        return m.group(0) if m else ''
+        m = re.match(r"\{.*\}", element.tag)
+        return m.group(0) if m else ""
 
     def GetPath(self, textPath):
         return os.path.join(textPath)
@@ -43,7 +38,7 @@ class WinModulesCollector(object):
             return False
 
     def GetModuleNameFromPath(self, path):
-        return path.replace(self.MODULESPATH + '\\', '')
+        return path.replace(self.MODULESPATH + "\\", "")
 
     def GetModulePathFromName(self, moduleName):
         return os.path.join(self.MODULESPATH, moduleName)
@@ -53,14 +48,14 @@ class WinModulesCollector(object):
         listOfModulesPaths = []
         print(self.vcxprojectFilePath)
         # TODO change to the self.vcxprojectFilePath
-        tree = ET.parse(os.path.abspath('AddOn.vcxproj'))
+        tree = ET.parse(os.path.abspath("AddOn.vcxproj"))
         root = tree.getroot()
         namespace = self.Namespace(root)
         AdditionalIncludeDirectories = root.findall(
-            f'{namespace}ItemDefinitionGroup/{namespace}ClCompile/{namespace}AdditionalIncludeDirectories'
+            f"{namespace}ItemDefinitionGroup/{namespace}ClCompile/{namespace}AdditionalIncludeDirectories"
         )
         for includeDirectory in AdditionalIncludeDirectories:
-            listOfModules = includeDirectory.text.split(';')
+            listOfModules = includeDirectory.text.split(";")
             listOfModulesPaths = map(self.GetPath, listOfModules)
             listOfModulesPaths = filter(self.FilterPaths, listOfModulesPaths)
 
@@ -69,29 +64,40 @@ class WinModulesCollector(object):
                     modulesList.append(self.GetModuleNameFromPath(modulePath))
         return modulesList
 
-class CMakeListModuleIncluder(WinModulesCollector):
-    def __init__(self, devKitPath, includePath, includeLibPath, cmakeFilePath, vcxprojectFolder):
-        super().__init__(devKitPath, includePath, includeLibPath, cmakeFilePath, vcxprojectFolder)
-    
+
+class CMakeListAutoModuleIncluder(WinModulesCollector):
+    def __init__(self, devKitPath, cmakeFilePath, vcxprojectFolder):
+        super().__init__(devKitPath, cmakeFilePath, vcxprojectFolder)
+
     def ParseCMakeFile(self):
+        with open(self.cmakeFilePath) as cmfile:
+            for line in cmfile:
+                print(line)
 
-
+    def GetCmakeExistingModules(self):
+        pass
 
 
 def Main(argv):
-    parser = argparse.ArgumentParser(description='CMakeList.txt module collector')
-    parser.add_argument('devKitPath', help = 'Path to the Archicad Development Kit')
-    parser.add_argument('cmakefilepath', help = 'Path to the CMakeLists.txt')
-    parser.add_argument('vcxprojfilepath', help = 'The path to the AddOn.vcxproj')
+    parser = argparse.ArgumentParser(description="CMakeList.txt module collector")
+    parser.add_argument(
+        "-devKitPath", "--devKitPath", help="Path to the Archicad Development Kit"
+    )
+    parser.add_argument(
+        "-cmakeFilePath", "--cmakeFilePath", help="Path to the CMakeLists.txt"
+    )
+    parser.add_argument(
+        "-vcxprojectFolder", "--vcxprojectFolder", help="The path to the AddOn.vcxproj"
+    )
     args = parser.parse_args()
 
-    comp = WinModulesCollector(
-        'C:\Program Files\GRAPHISOFT\API Development Kit 24.3009',
-        '2',
-        '3',
-        '4',
-        'CMAKE_BINARY_DIR',
+    # python AddModules.py -devKitPath "C:\Program Files\GRAPHISOFT\API Development Kit 24.3009" -cmakeFilePath "D:\TEMP\CPP DEVELOPMENT\archicad-addon-cmake" -vcxprojectFolder "D:\TEMP\CPP DEVELOPMENT\archicad-addon-cmake\Build"
+    moduleIncluder = CMakeListAutoModuleIncluder(
+        devKitPath=args.devKitPath,
+        cmakeFilePath=args.cmakeFilePath,
+        vcxprojectFolder=args.vcxprojectFolder,
     )
+    moduleIncluder.ParseCMakeFile()
     # comp.GetExistingProjectModules()
 
     return 0
