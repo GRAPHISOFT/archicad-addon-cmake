b541bb55cb1db4462ccfdcbc52af050c668c40fd
Akos Nagy <AkNagy@graphisoft.com>
2024-08-16 16:16:23 +0200

    Modify the example dialog related codes and resources to favorite converter dialog related ones.
    // Resizing is disabled due to undefined behavior -- waiting for design

--- Src/AddOnMain.cpp
+++ Src/AddOnMain.cpp
@@ -1,78 +1,22 @@
-#include "APIEnvir.h"
-#include "ACAPinc.h"
+#include "FavoriteConverterDialog.hpp"
 
-#include "ResourceIds.hpp"
-#include "DGModule.hpp"
 
 static const GSResID AddOnInfoID			= ID_ADDON_INFO;
 	static const Int32 AddOnNameID			= 1;
 	static const Int32 AddOnDescriptionID	= 2;
 
-static const short AddOnMenuID				= ID_ADDON_MENU;
-	static const Int32 AddOnCommandID		= 1;
+static const short FavoriteConverterMenuID			= ID_FAVORITE_CONVERTER_MENU;
+	static const Int32 FavoriteConverterSubMenuID	= 1;
 
-class ExampleDialog :	public DG::ModalDialog,
-						public DG::PanelObserver,
-						public DG::ButtonItemObserver,
-						public DG::CompoundItemObserver
-{
-public:
-	enum DialogResourceIds
-	{
-		ExampleDialogResourceId = ID_ADDON_DLG,
-		OKButtonId = 1,
-		CancelButtonId = 2,
-		SeparatorId = 3
-	};
-
-	ExampleDialog () :
-		DG::ModalDialog (ACAPI_GetOwnResModule (), ExampleDialogResourceId, ACAPI_GetOwnResModule ()),
-		okButton (GetReference (), OKButtonId),
-		cancelButton (GetReference (), CancelButtonId),
-		separator (GetReference (), SeparatorId)
-	{
-		AttachToAllItems (*this);
-		Attach (*this);
-	}
-
-	~ExampleDialog ()
-	{
-		Detach (*this);
-		DetachFromAllItems (*this);
-	}
-
-private:
-	virtual void PanelResized (const DG::PanelResizeEvent& ev) override
-	{
-		BeginMoveResizeItems ();
-		okButton.Move (ev.GetHorizontalChange (), ev.GetVerticalChange ());
-		cancelButton.Move (ev.GetHorizontalChange (), ev.GetVerticalChange ());
-		separator.MoveAndResize (0, ev.GetVerticalChange (), ev.GetHorizontalChange (), 0);
-		EndMoveResizeItems ();
-	}
-
-	virtual void ButtonClicked (const DG::ButtonClickEvent& ev) override
-	{
-		if (ev.GetSource () == &okButton) {
-			PostCloseRequest (DG::ModalDialog::Accept);
-		} else if (ev.GetSource () == &cancelButton) {
-			PostCloseRequest (DG::ModalDialog::Cancel);
-		}
-	}
-
-	DG::Button		okButton;
-	DG::Button		cancelButton;
-	DG::Separator	separator;
-};
 
 static GSErrCode MenuCommandHandler (const API_MenuParams *menuParams)
 {
 	switch (menuParams->menuItemRef.menuResID) {
-		case AddOnMenuID:
+		case FavoriteConverterMenuID:
 			switch (menuParams->menuItemRef.itemIndex) {
-				case AddOnCommandID:
+				case FavoriteConverterSubMenuID:
 					{
-						ExampleDialog dialog;
+						FC::Dialog dialog;
 						dialog.Invoke ();
 					}
 					break;
@@ -82,31 +26,27 @@ static GSErrCode MenuCommandHandler (const API_MenuParams *menuParams)
 	return NoError;
 }
 
+
 API_AddonType CheckEnvironment (API_EnvirParams* envir)
 {
-	RSGetIndString (&envir->addOnInfo.name, AddOnInfoID, AddOnNameID, ACAPI_GetOwnResModule ());
-	RSGetIndString (&envir->addOnInfo.description, AddOnInfoID, AddOnDescriptionID, ACAPI_GetOwnResModule ());
+	envir->addOnInfo.name = RSGetIndString (AddOnInfoID, AddOnNameID, ACAPI_GetOwnResModule ());
+	envir->addOnInfo.description = RSGetIndString (AddOnInfoID, AddOnDescriptionID, ACAPI_GetOwnResModule ());
 
 	return APIAddon_Normal;
 }
 
+
 GSErrCode RegisterInterface (void)
 {
-#ifdef ServerMainVers_2700
-	return ACAPI_MenuItem_RegisterMenu (AddOnMenuID, 0, MenuCode_Tools, MenuFlag_Default);
-#else
-	return ACAPI_Register_Menu (AddOnMenuID, 0, MenuCode_Tools, MenuFlag_Default);
-#endif
+	return ACAPI_MenuItem_RegisterMenu (FavoriteConverterMenuID, 0, MenuCode_Tools, MenuFlag_Default);
 }
+
 
 GSErrCode Initialize (void)
 {
-#ifdef ServerMainVers_2700
-	return ACAPI_MenuItem_InstallMenuHandler (AddOnMenuID, MenuCommandHandler);
-#else
-	return ACAPI_Install_MenuHandler (AddOnMenuID, MenuCommandHandler);
-#endif
+	return ACAPI_MenuItem_InstallMenuHandler (FavoriteConverterMenuID, MenuCommandHandler);
 }
+
 
 GSErrCode FreeData (void)
 {
---
+++ Src/FavoriteConverterDialog.cpp
@@ -0,0 +1,42 @@
+#include "FavoriteConverterDialog.hpp"
+
+namespace FC {
+
+Dialog::Dialog () :
+    DG::ModalDialog (ACAPI_GetOwnResModule (), DialogResourceId, ACAPI_GetOwnResModule ()),
+    convertButton (GetReference (), ConvertButtonId),
+    cancelButton (GetReference (), CancelButtonId),
+    separator (GetReference (), SeparatorId)
+{
+    AttachToAllItems (*this);
+    Attach (*this);
+}
+
+
+Dialog::~Dialog ()
+{
+    Detach (*this);
+    DetachFromAllItems (*this);
+}
+
+
+void Dialog::PanelResized (const DG::PanelResizeEvent& /* ev */)
+{
+    // BeginMoveResizeItems ();
+    // convertButton.Move (ev.GetHorizontalChange (), ev.GetVerticalChange ());
+    // cancelButton.Move (ev.GetHorizontalChange (), ev.GetVerticalChange ());
+    // separator.MoveAndResize (0, ev.GetVerticalChange (), ev.GetHorizontalChange (), 0);
+    // EndMoveResizeItems ();
+}
+
+
+void Dialog::ButtonClicked (const DG::ButtonClickEvent& ev)
+{
+    if (ev.GetSource () == &convertButton) {
+        PostCloseRequest (DG::ModalDialog::Accept);
+    } else if (ev.GetSource () == &cancelButton) {
+        PostCloseRequest (DG::ModalDialog::Cancel);
+    }
+}
+
+} // namespace FC
