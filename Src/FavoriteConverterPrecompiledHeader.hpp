cdddb6fdb6f8eefaaaa7a60cf2c32752f856bb00
Akos Nagy <AkNagy@graphisoft.com>
2024-08-16 15:53:34 +0200

    Add precompiled header file to cmake.

---
+++ Src/FavoriteConverterPrecompiledHeader.hpp
@@ -0,0 +1,20 @@
+#ifndef GS_FAVORITE_CONVERTER_PRECOMPILED_HEADER_HPP
+#define GS_FAVORITE_CONVERTER_PRECOMPILED_HEADER_HPP
+
+#include <limits.h>
+#include <math.h>
+#include <stddef.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#if defined(WINDOWS)
+#include "Win32Interface.hpp"
+#endif
+
+#include "APIEnvir.h"
+#include "ACAPinc.h"
+
+GS_UNHANDLED_ENUM_VALUE_CHECK_ON
+
+#endif // GS_FAVORITE_CONVERTER_PRECOMPILED_HEADER_HPP
